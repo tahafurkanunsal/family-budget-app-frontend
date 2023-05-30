1ca5ade3fd8b63d10a00625a3c7e48a9a6c51b22
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-05-30 15:16:04 +0300

    Created component to create user

---
+++ src/app/components/add-users/add-users.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/services/user.service';
+
+@Component({
+  selector: 'app-add-users',
+  templateUrl: './add-users.component.html',
+  styleUrls: ['./add-users.component.css']
+})
+export class AddUsersComponent implements OnInit {
+
+  data : any;
+  form: FormGroup;
+
+  constructor(private userService : UserService , private router: Router) {
+      this.form = new FormGroup({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required])
+    });
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+  submit(){
+    if (this.form.valid) {
+      const data = this.form.value;
+      console.log(data);
+
+      this.userService.create(data).subscribe(response => {
+        console.log(response);
+        this.router.navigate(['/']); // Redirect to home page after successful submission
+      });
+  }
+ }
+}
