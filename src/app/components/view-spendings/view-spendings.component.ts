9f1d06fc34139785d349e8aa62b42f9e570f4c47
Taha Furkan Ünsal <t.furkanunsal@gmail.com>
2023-06-18 14:15:58 +0300

    The redirects of the components on the pages have been edited.

--- src/app/components/view-spendings/view-spendings.component.ts
+++ src/app/components/view-spendings/view-spendings.component.ts
@@ -1,48 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SpendingService } from 'src/services/spending.service';
 import { Spending } from 'src/model/Spending';
 import { formatDate } from '@angular/common';
 import { UserService } from 'src/services/user.service';
-
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-view-spendings',
   templateUrl: './view-spendings.component.html',
   styleUrls: ['./view-spendings.component.css']
 })
-export class ViewSpendingsComponent implements OnInit {
-
+export class ViewSpendingsComponent implements OnInit, AfterViewInit, OnDestroy {
   spendings: Spending[] = [];
   startDate: Date = new Date();
   endDate: Date = new Date();
   userId: number | undefined;
-  
-  constructor(private spendingService: SpendingService, private router: Router , private userService: UserService) {}
+  spendingByMonth: { month: string, amount: number }[] = [];
+  spendingChart: Chart | undefined;
+
+  constructor(
+    private spendingService: SpendingService,
+    private router: Router,
+    private userService: UserService
+  ) {
+    Chart.register(...registerables);
+  }
 
   ngOnInit() {
     this.loadAllSpendings();
   }
 
-  loadAllSpendings() {
-    this.spendingService.getAllSpendings().subscribe(spendings => {
-      this.spendings = spendings;
-    });
+  ngAfterViewInit() {
   }
 
-  getSpendingsByDate() {
-    const formattedStartDate = formatDate(this.startDate, 'yyyy-MMM-dd', 'en-US');
-    const formattedEndDate = formatDate(this.endDate, 'yyyy-MMM-dd', 'en-US');
+  ngOnDestroy() {
+    if (this.spendingChart) {
+      this.spendingChart.destroy();
+    }
+  }
+
+loadAllSpendings() {
+  this.spendingService.getAllSpendings().subscribe(spendings => {
+    this.spendings = spendings;
+  });
+}
+
   
+  
+  getMonthName(monthIndex: number): string {
+    const months = ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'];
+    return months[monthIndex];
+  }
+   
+
+  getSpendingsByDate() {
+    const formattedStartDate = formatDate(this.startDate, 'yyyy-MM-dd', 'tr-TR');
+    const formattedEndDate = formatDate(this.endDate, 'yyyy-MM-dd', 'tr-TR');
+
     this.spendingService.getSpendingsByDate(formattedStartDate, formattedEndDate).subscribe(spendings => {
       this.spendings = spendings;
     });
   }
-  
 
   addSpending(spending: Spending) {
-    this.router.navigate(['/add'], { state: { spending } });
+    this.spendingService.createSpending(spending).subscribe(() => {
+      this.loadAllSpendings();
+    });
   }
+
   showDetails(spending: Spending) {
     spending.showDetails = !spending.showDetails;
   }
@@ -60,10 +86,12 @@ export class ViewSpendingsComponent implements OnInit {
       });
     }
   }
+
   clearFilter() {
     this.startDate = new Date();
     this.endDate = new Date();
     this.userId = undefined;
     this.loadAllSpendings();
   }
+
 }
